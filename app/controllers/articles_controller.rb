e95119f38c5d22a5171b7523ebccfd7efb6f23c3
Visvaldas <visva.rapalis@gmail.com>
2020-10-05 17:09:37 +0300

    Finish articles crud operations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,52 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
-  def edit
+  def create
+    @article = Article.create(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
   end
+  
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+
+    @article.destroy
+    redirect_to :action => :index
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
 end
