72e44f3fe478d90a1bfa3b94901ef1f72b03813f
Visvaldas <visva.rapalis@gmail.com>
2020-10-05 17:42:13 +0300

    Add functionality to choose category when creating article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -46,7 +46,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :content)
+    params.require(:article).permit(:title, :content, :category_id)
   end
 
 end
