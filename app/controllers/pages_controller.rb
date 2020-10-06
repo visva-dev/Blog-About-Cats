19e8e5c91f999179fdddcdadcd9c03562a6e3ce8
Visvaldas <visva.rapalis@gmail.com>
2020-10-06 16:51:00 +0300

    Add categories menu

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
   def home
-    
+    @categories = Category.all
+
+    cate = params[:cate]
+
+    if !cate.nil?
+      @articles = Article.where(:category_id => cate)
+    else
+      @articles = Article.all
+    end
   end
   
 end
