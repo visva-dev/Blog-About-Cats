67c230aa9d45ab50beb8140dc2eebf6fb31a12d4
Visvaldas <visva.rapalis@gmail.com>
2020-10-14 15:55:40 +0300

    Delete 2 spaces in articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
                   Article.where(category_id: cate).includes([:user]).paginate(page: params[:page], per_page: 3)
                 else
                   @articles = Article.order(created_at: :desc).includes(:user)
-                            .includes([:user]).paginate(page: params[:page], per_page: 3)
+                        .includes([:user]).paginate(page: params[:page], per_page: 3)
                 end
   end
 
