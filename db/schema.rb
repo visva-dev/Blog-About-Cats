3c4bcbee8c566331253a9374c1ebfa170eaabec5
Visvaldas <visva.rapalis@gmail.com>
2020-10-15 14:20:21 +0300

    Add priority to categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_110638) do
+ActiveRecord::Schema.define(version: 2020_10_15_111721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_110638) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "priority"
   end
 
   create_table "likes", force: :cascade do |t|
