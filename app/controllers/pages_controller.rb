c4fea87e33b8c2ec4158b17cff4a7af4e0210024
Visvaldas <visva.rapalis@gmail.com>
2020-10-19 12:00:33 +0300

    Fix rubocop errors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,10 @@ class PagesController < ApplicationController
     cate = params[:cate]
     @articles = if !cate.nil?
                   Article.where(category_id: cate).includes([:user]).order(created_at: :desc)
-                         .paginate(page: params[:page], per_page: 3)
+                    .paginate(page: params[:page], per_page: 3)
                 else
                   @articles = Article.order(created_at: :desc)
-                                     .includes([:user]).paginate(page: params[:page], per_page: 3)
+                    .includes([:user]).paginate(page: params[:page], per_page: 3)
                 end
   end
 end
